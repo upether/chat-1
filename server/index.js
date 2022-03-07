7b073c8848a9055bd381816e94b82dcb96557e94
JSC <gbs04087@gmail.com>
2022-03-07 18:25:23 +0900

    useState prev

--- server/index.js
+++ server/index.js
@@ -14,14 +14,20 @@ io.on("connection", (socket) => {
   socket.on("join Room", async ({ roomId }) => {
     console.log("join Room")
     console.log(roomId);
+    // 소켓이 들어가있는 룸들을 다 disconnect
+    console.log(socket.rooms)
     for (let room of [...socket.rooms].slice(1)) {
       socket.leave(room);
     }
+    // 새로 들어온 roomid 로 join
     socket.join(roomId);
+    console.log(socket.rooms)
+
   });
-  socket.on("message", ({ roomId, name, message }) => {
-    console.log(roomId, name, message);
-    io.to(roomId).emit("message", { name, message });
+  socket.on("message", ({ name, message }) => {
+    console.log(name, message);
+    // 받은 roomId로 이름과 메시지를 전송
+    io.to([...socket.rooms][1]).emit("message", { name, message });
   });
   socket.on("disconnect", () => {
     console.log("disconnect")
